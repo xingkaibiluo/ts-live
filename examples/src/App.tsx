7da05c155cbd9e636deaa7af1e8fd2a72420c81f
dcx <duanchuanxu@bytedance.com>
2020-03-26 15:18:57 +0800

    examples

---
+++ examples/src/App.tsx
@@ -0,0 +1,32 @@
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import ReactDOM from 'react-dom';
+import {
+  PreviewerProvider,
+  Previewer,
+  PreviewerEditor,
+  PreviewerError
+} from '@byte-design/react-previewer';
+import logo from './logo.svg';
+import './App.css';
+import '@byte-design/react-previewer/style/index.css'
+
+
+function App() {
+
+  return (
+    <PreviewerProvider
+      code={`
+let num: number = 123;
+export default function Demo(){
+    return '1234'
+}
+      `}
+    >
+      <Previewer />
+      <PreviewerEditor width="500px" height="300px" />
+      <PreviewerError />
+    </PreviewerProvider>
+  );
+}
+
+export default App;

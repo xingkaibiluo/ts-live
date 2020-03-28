c2f042aa542874573cbfaa05a6e40b58e52c5307
dcx <duanchuanxu@bytedance.com>
2020-03-28 21:17:31 +0800

    monaco worker & build worker

--- packages/ts-editor/src/index.ts
+++ packages/ts-editor/src/index.ts
@@ -5,50 +5,6 @@ export {
 } from './lib/Editor';
 
 import * as monaco from 'monaco-editor';
-// import "monaco-editor/esm/vs/editor/editor.worker";
-// import "monaco-editor/esm/vs/language/json/json.worker";
-// import "monaco-editor/esm/vs/language/css/css.worker";
-// import "monaco-editor/esm/vs/language/html/html.worker";
+
 export {monaco};
 
-const jsonWorker = require("monaco-editor/esm/vs/language/json/json.worker.js")
-const tsWorker = require("monaco-editor/esm/vs/language/typescript/ts.worker.js")
-const cssWorker = require("monaco-editor/esm/vs/language/css/css.worker.js")
-const htmlWorker = require("monaco-editor/esm/vs/language/html/html.worker.js")
-const Worker = require("monaco-editor/esm/vs/editor/editor.worker")
-
-// @ts-ignore
-window.MonacoEnvironment = {
-    // getWorker: (moduleId: string, label: string) => {
-    //     console.log('-------------getWorker', moduleId, label)
-    //     if (label === "json") {
-    //         return new jsonWorker();
-    //     }
-    //     if (label === "css") {
-    //         return new cssWorker();
-    //     }
-    //     if (label === "html") {
-    //         return new htmlWorker();
-    //     }
-    //     if (label === "typescript" || label === "javascript") {
-    //         console.log('-------------getWorker typescript', tsWorker)
-    //         return new tsWorker();
-    //     }
-    //     return new Worker();
-    // },
-    getWorkerUrl: function (moduleId: string, label: string) {
-        if (label === "json") {
-            return "./worker/json.worker.js";
-        }
-        if (label === "css") {
-            return "./worker/css.worker.js";
-        }
-        if (label === "html") {
-            return "./worker/html.worker.js";
-        }
-        if (label === "typescript" || label === "javascript") {
-            return "./worker/ts.worker.js";
-        }
-        return "./worker/editor.worker.js";
-    }
-};
